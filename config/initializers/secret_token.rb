eddec6d8dbcd7bd735f76d8bf4683d97ba83752d
Trevor Kimenye <kimenye@gmail.com>
2012-12-31 11:10:52 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Totomama::Application.config.secret_token = 'fe81500df8d9d6f6a2d8f4fb36d8d6c8139cce16fde0c9393b13835e3f03dca0c8c26ce601c0e89f99cb3fafb58ee38e2418131abaae6760404aa7f0d97514ea'
