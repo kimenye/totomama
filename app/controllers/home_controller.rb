ee89e455215394fe6bbafaf936235b46579bcf05
Trevor Kimenye <kimenye@gmail.com>
2013-01-02 16:24:56 +0300

    Initial take on the product dropdwons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @categories = Category.all
+    categories = Category.all
+
+    @categories = []
+    categories.each do |c|
+      @categories <<  {
+          :category => c,
+          :products => Product.find_all_by_category_id(c.id)
+      }
+    end
   end
 end
