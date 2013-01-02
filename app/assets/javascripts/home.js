acb6b4b05c845edac8b85cff6fa997682c69a00d
Trevor Kimenye <kimenye@gmail.com>
2013-01-02 19:22:24 +0300

    Disabled checkboxes from being selected

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $.each($('.rating-readonly'), function(index, rating) {
+        var val = $.attr(rating, 'data-rating');
+        var type = $.attr(rating, 'data-type');
+        var item_id = $.attr(rating, 'data-id');
+        var selector = "star" + val + "-product-" + item_id;
+        $("#" + selector).attr('checked', 'checked');
+    });
+
+    $('.rating-readonly :radio').attr('disabled', 'disabled')
+});
