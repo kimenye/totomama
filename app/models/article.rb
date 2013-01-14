8b85b741fa16a876568f0bfcdcb574d1b341ae86
Joyce Echessa <jokhessa@yahoo.com>
2013-01-14 16:45:06 +0300

    Changed search result page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   belongs_to :admin
   belongs_to :category
-  attr_accessible :content, :status, :title, :admin_id
+  attr_accessible :content, :status, :title, :admin_id, :category_id
 end
