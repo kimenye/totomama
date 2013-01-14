4f5dac45a4f18ac213b7aac5a65038c2efcd7df5
Joyce Echessa <jokhessa@yahoo.com>
2013-01-14 09:38:37 +0300

    Fixed the way products, shops and events were categorised

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     product_categories
-    @popular_products = Item.order("impressions_count DESC").limit(6)
-    @popular_shops = Shop.order("impressions_count DESC").limit(6)
-    @popular_events = Event.order("impressions_count DESC").limit(6)
+    @popular_products = Item.where('type = ?', 'Item').order("impressions_count DESC").limit(6)
+    @popular_shops = Shop.where('type = ?', 'Shop').order("impressions_count DESC").limit(6)
+    @popular_events = Event.where('type = ?', 'Event').order("impressions_count DESC").limit(6)
   end
 
 end
